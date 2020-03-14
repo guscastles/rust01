5c6c505ecbd819b2dc7a80159ac8886a606f66ec
guscastles <guscastles@gmail.com>
2020-03-14 20:13:27 +1100

    Added modules. WiP

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,8 @@
 /// The player has 5 chances to guess a
 /// random number
 
-use rand::Rng;
+use guessing_game::engine;
 
 fn main() {
-    play_game()
-}
-
-fn play_game() {
-    println!("Please guess a number from 1 to 10:");
-    let guess = rand::thread_rng().gen_range(1, 10);
-    println!("Secret number is {}", guess)
+    game::play_game()
 }
