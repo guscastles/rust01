f947af3a82306030cb37ec6515be19264f0d6ca6
guscastles <guscastles@gmail.com>
2020-03-18 11:12:30 +1100

    Added newmath library. Updated module comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-/// Guessing Game
-/// Experimental code in Rust
-/// The player has 5 chances to guess a
-/// random number
+//! Guessing Game
+//! Experimental code in Rust
+//! The player has 5 chances to guess a random number.
 
 mod engine;
+use newmath;
 
 fn main() {
+    println!("This is the function from the newmath module: {}",
+             newmath::addition_plus_one(1, 2));
     engine::play()
 }
